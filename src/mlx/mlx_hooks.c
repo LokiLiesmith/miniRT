d07605d2393b6417a20df584815ffa96176efc6e
Mark Razem <mrazem@3-D-10.42heilbronn.de>
2025-11-21 03:35:48 +0100

    mouse_camera

--- src/mlx/mlx_hooks.c
+++ src/mlx/mlx_hooks.c
@@ -6,7 +6,7 @@
 /*   By: mrazem <mrazem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 20:10:32 by djanardh          #+#    #+#             */
-/*   Updated: 2025/11/21 01:07:10 by mrazem           ###   ########.fr       */
+/*   Updated: 2025/11/21 03:26:56 by mrazem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -210,11 +210,19 @@ void	mouse_drag(mouse_key_t button, action_t action, modifier_key_t mods, void *
 	if (button == MLX_MOUSE_BUTTON_MIDDLE && action == MLX_PRESS)
 		rt->pan_drag = true;
 	if (button == MLX_MOUSE_BUTTON_MIDDLE && action == MLX_RELEASE)
+	{
 		rt->pan_drag = false;
+		rt->samples = 250;
+		render(rt);
+	}
 	if (button == MLX_MOUSE_BUTTON_RIGHT && action == MLX_PRESS)
 		rt->rotate_drag = true;
 	if (button == MLX_MOUSE_BUTTON_RIGHT && action == MLX_RELEASE)
-		rt->rotate_drag = false;
+	{
+			rt->rotate_drag = false;
+			rt->samples = 250;
+			render(rt);
+	}	
 }
 
 void	mouse_pan(t_rt *rt, t_view view)
@@ -223,7 +231,7 @@ void	mouse_pan(t_rt *rt, t_view view)
 	double			speed;
 
 	m = init_mouse(rt);
-	speed = 0.002;
+	speed = 0.02;
 	
 	rt->scene.camera.pos = vec_add(rt->scene.camera.pos, vec_add(vec_scale(view.right, speed * -m.dx), vec_scale(view.up, speed * m.dy)));
 
@@ -238,9 +246,9 @@ void	mouse_rotate(t_rt *rt, t_view view)
 
 	(void)view;
 	m = init_mouse(rt);
-	angle = 0.005;
+	angle = 0.0005;
 
-    rt->scene.camera.dir = vec_rotate_y(rt->scene.camera.dir, -m.dx * angle);
+    rt->scene.camera.dir = vec_rotate_y(rt->scene.camera.dir, m.dx * angle);
     rt->scene.camera.dir = vec_rotate_x(rt->scene.camera.dir, -m.dy * angle);
 	rt->scene.camera.dir = vec_normalize(rt->scene.camera.dir);
 }
@@ -249,13 +257,10 @@ void drag_loop(void *param)
 {
 	t_rt 			*rt;
 	t_view			view;
-	t_mouse_data	m;
+	// t_mouse_data	m;
 	
 	rt= (t_rt *)param;
 	view = camera_orientation(rt);
-	m = init_mouse(rt);
-	rt->prev_mouse_x = m.mx;
-	rt->prev_mouse_y = m.my;
 	if (!rt->pan_drag && !rt->rotate_drag)
 		return ;
 	if (rt->pan_drag)
